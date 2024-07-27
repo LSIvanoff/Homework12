d8896f7b5a5a2aa6b980226b5e9e86119cccdc4f
Neytred <ls.ivanova91@gmail.com>
2024-07-27 16:11:55 +0900

    ДЗ 12 сделано

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ public class Main {
 
         Book book1 = new Book("ЭтноВзгляд", author1, 2023);
         System.out.println("Название: " + book1.getTitle());
-        System.out.println("Автор: " + author1);
+        System.out.println("Автор: " + book1.getAuthor().getLastname() + " " + book1.getAuthor().getName());
         System.out.println("Дата публикации: " + book1.getPublishDate());
         book1.setPublishDate(2013);
         System.out.println("Дата публикации: " + book1.getPublishDate() + "\n");
 
         Author author2 = new Author("Платон", "Ойунский");
         System.out.println("Автор: " + author2.getLastname() + " " + author2.getName());
-        Book book2 = new Book("ЭтноВзгляд", author2, 2023);
+        Book book2 = new Book("Нюргун Боотур Стремительный", author2, 1947);
         System.out.println("Название: " + book2.getTitle());
-        System.out.println("Автор: " + author2);
+        System.out.println("Автор: " + book2.getAuthor().getLastname() + " " + book2.getAuthor().getName());
         System.out.println("Дата публикации: " + book2.getPublishDate());
-        book2.setPublishDate(2013);
+        book2.setPublishDate(1970);
         System.out.println("Дата публикации: " + book2.getPublishDate() + "\n");
     }
 
